93fae1d6988f37e9a72513cc68aad1c3b080b734
CedricGuessou <121789165+CedricGuessou@users.noreply.github.com>
2023-06-22 23:41:30 +0000

    Did a bunch of stuff that will make the Romi move a lot

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -17,6 +17,30 @@ public class Robot extends ProceduralRobot {
   @Override
   public void autonomousProcedure() throws InterruptedException {
     // Write your code here:
+    System.out.println("First movement phase: ");
+    driveTime(0.6, 2);
+    System.out.println("End of first movement phase!");
+    sleepSeconds(1);
+    System.out.println("First rotation phase: ");
+    turnTime(.2, 2);
+    System.out.println("End of first rotation phase!");
+    sleepSeconds(.7);
+    System.out.println("Second movement phase: ");
+    driveTime(0.4, 5);
+    System.out.println("End of second movement phase!");
+    sleepSeconds(1);
+    System.out.println("Second rotation phase: ");
+    turnTime(0.175, 2);
+    System.out.println("End of second rotation phase!");
+    sleepSeconds(.7);
+    System.out.println("Third movement phase: ");
+    driveTime(0.5, 4);
+    System.out.println("End of third movement phase!");
+    sleepSeconds(3);
+    System.out.println("VICTORY SPIN!!!");
+    turnTime(0.7, 3);
+    turnTime(-0.7, 4);
+    System.out.println("Thank you!");
 
   }
 }
